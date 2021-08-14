8d9baf4f1ea4e1c5e4432adb67cd133fe93bf0d1
uniquenikki <59661115+uniquenikki@users.noreply.github.com>
2021-08-14 10:41:24 +0530

    Update Register.js

--- src/Register.js
+++ src/Register.js
@@ -31,37 +31,37 @@ const handleSubmit=(e)=>
 
    return(
         <>
-        <Container className='box px-5'>
-        <h1 className='p-5'>REGISTER</h1>
-         <form action="" onSubmit={handleSubmit}>
-             <div className='inputData'>
+        <Container className='wrapper px-5'>
+        <h1 className='p-5 title'>REGISTER</h1>
+         <form className='form' action="" onSubmit={handleSubmit}>
+             <div className='inputfield'>
              <label htmlFor='username'>Full Name</label>
-             <input className='inputField' type='text' autoComplete="off" 
+             <input className='input' type='text' autoComplete="off" 
               value={userRegistration.username} onChange={handleInput} name="username" id='username'></input>
              </div>
-             <div className='inputData'>
-             <label htmlFor='email'>Email</label>
-             <input className='inputField' type='email' autoComplete="off" 
+             <div className='inputfield'>
+             <label htmlFor='email' className="leading-7 text-sm">Email</label>
+             <input className='input' type='email' autoComplete="off" 
             value={userRegistration.email} onChange={handleInput} name="email" id='email'></input>
              </div>
-             <div className='inputData'>
-             <label htmlFor='phone'>Phone</label>
-             <input className='inputField' type='number' autoComplete="off"
+             <div className='inputfield'>
+             <label htmlFor='phone' className="leading-7 text-sm">Phone</label>
+             <input className='input' type='number' autoComplete="off"
             value={userRegistration.phone} onChange={handleInput} 
              name="phone" id='phone'></input>
              </div>
-             <div className='inputData'>
+             <div className='inputfield'>
              <label htmlFor='password'>Password</label>
-             <input className='inputField' type='password' autoComplete="off" 
+             <input className='input' type='password' autoComplete="off" 
             value={userRegistration.password} onChange={handleInput} name="password" id='password'></input>
              </div>
              <div classname="p-5 mx-3">
-             <Button  onClick={()=>history.push('/AddProduct')} type='submit'>SUBMIT</Button>
+             <Button onClick={()=>history.push('/AddProduct')} type='submit'>SUBMIT</Button>
              {/* <Button type='submit'>SUBMIT</Button> */}
              </div>
             </form> 
-            
             </Container>
+            
             
      </>
     )      
